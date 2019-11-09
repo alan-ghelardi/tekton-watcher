25b0f3ff6a4780910b9c75494f48639707bd2189
Alan Ghelardi <alan.ghelardi@gmail.com>
2019-11-09 18:06:53 -0200

    Refactor code to call new config values.

--- src/tekton_watcher/pulls.clj
+++ src/tekton_watcher/pulls.clj
@@ -1,6 +1,12 @@
 (ns tekton-watcher.pulls
   (:require [tekton-watcher.api :refer [defsub]]
-            [tekton-watcher.http-client :as http-client]))
+            [tekton-watcher.http-client :as http-client]
+            [tekton-watcher.misc :as misc]))
+
+(defn- link-to-task-run-on-dashboard
+  [task-run config]
+  (misc/render (config :tekton.dashboard/task-run)
+               {:task-run (get-in task-run [:metadata :name])}))
 
 (defn- get-sha
   [git-resource]
@@ -9,23 +15,22 @@
                (get-in git-resource [:spec :params]))))
 
 (defn- get-git-resource
-  [{:cluster/keys [host namespace]} task-run]
+  [task-run {:tekton.api/keys [url]}]
   (->> task-run
        :spec
        :inputs
        :resources
        (map #(get-in % [:resourceRef :name]))
-       (map #(http-client/send-and-await #:http{:url         "{host}/apis/tekton.dev/v1alpha1/namespaces/{namespace}/pipelineresources/{resource-name}"
-                                                :path-params {:host          host
-                                                              :namespace     namespace
+       (map #(http-client/send-and-await #:http{:url         "{url}/pipelineresources/{resource-name}"
+                                                :path-params {:url           url
                                                               :resource-name %}}))
        (filter #(get-in % [:metadata :annotations "tekton-watcher/statuses-url"]))
        first))
 
 (defn update-commit-status
-  [{:github/keys [oauth-token] :as config} task-run {:status/keys [state description]}]
+  [task-run {:github/keys [oauth-token] :as config} {:status/keys [state description]}]
   (let [task-name    (get-in task-run [:metadata :labels "tekton.dev/pipelineTask"])
-        git-resource (get-git-resource config task-run)]
+        git-resource (get-git-resource task-run config)]
     (when git-resource
       (let [sha          (get-sha git-resource)
             statuses-url (get-in git-resource [:metadata :annotations "tekton-watcher/statuses-url"])]
@@ -34,15 +39,16 @@
                                        :oauth-token oauth-token
                                        :path-params {:sha sha}
                                        :payload     {:state       state
-                                                     :context     (str "nubank/ci-cd: " task-name)
-                                                     :description description}})))))
+                                                     :context     (str "nubank/cd: " task-name)
+                                                     :description description
+                                                     :target_url  (link-to-task-run-on-dashboard task-run config)}})))))
 
 (defsub run-started :task-run/running
   [task-run config]
-  (update-commit-status config task-run #:status{:state       "pending"
+  (update-commit-status task-run config #:status{:state       "pending"
                                                  :description "Tekton is running your tests..."}))
 
 (defsub run-succeeded :task-run/succeeded
   [task-run config]
-  (update-commit-status config task-run #:status{:state       "success"
+  (update-commit-status task-run config #:status{:state       "success"
                                                  :description "your tests passed on Tekton!"}))
--- src/tekton_watcher/runs.clj
+++ src/tekton_watcher/runs.clj
@@ -10,10 +10,9 @@
        (some #(= reason (:reason %)))))
 
 (defn- list-runs
-  [{:cluster/keys [host namespace]} label-selectors reason]
-  (->> (http-client/send-and-await #:http{:url          "{host}/apis/tekton.dev/v1alpha1/namespaces/{namespace}/taskruns"
-                                          :path-params  {:host      host
-                                                         :namespace namespace}
+  [{:tekton.api/keys [url]} label-selectors reason]
+  (->> (http-client/send-and-await #:http{:url          "{url}/taskruns"
+                                          :path-params  {:url url}
                                           :query-params {:labelSelector label-selectors}})
        :items
        (filter #(contains-reason? % reason))))
@@ -27,11 +26,11 @@
   (list-runs config "tekton-watcher/running-event-fired,!tekton-watcher/completed-event-fired" "Succeeded"))
 
 (defn- add-label
-  [{:cluster/keys [host]} run label-key]
+  [run {:api.server/keys [url]} label-key]
   (http-client/send-async #:http{:verb        :patch
-                                 :url         "{host}{link}"
+                                 :url         "{url}{link}"
                                  :produces    "application/json-patch+json"
-                                 :path-params {:host host
+                                 :path-params {:url  url
                                                :link (get-in run [:metadata :selfLink])}
                                  :payload     [{:op    "add"
                                                 :path  (str "/metadata/labels/tekton-watcher~1" label-key)
@@ -55,8 +54,8 @@
 
 (defsub run-started :task-run/running
   [task-run config]
-  (add-label config task-run "running-event-fired"))
+  (add-label task-run config "running-event-fired"))
 
 (defsub run-succeeded :task-run/succeeded
   [task-run config]
-  (add-label config task-run "completed-event-fired"))
+  (add-label task-run config "completed-event-fired"))
