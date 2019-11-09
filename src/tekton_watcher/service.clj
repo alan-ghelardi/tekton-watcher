a4ce09b35ec2cd1caeb5c760c1348febcb13f4d5
Alan Ghelardi <alan.ghelardi@gmail.com>
2019-11-09 18:06:33 -0200

    Use new config namespace.

--- src/tekton_watcher/service.clj
+++ src/tekton_watcher/service.clj
@@ -1,6 +1,7 @@
 (ns tekton-watcher.service
   (:require [clojure.tools.logging :as log]
             [tekton-watcher.api :as api]
+            [tekton-watcher.config :as config]
             [tekton-watcher.pulls :as pulls]
             [tekton-watcher.runs :as runs]))
 
@@ -14,15 +15,9 @@
    pulls/run-started
    pulls/run-succeeded])
 
-(defn read-config
-  []
-  {:cluster/host       "http://localhost:8080"
-   :cluster/namespace  "default"
-   :github/oauth-token (slurp "/etc/github-statuses-updater/oauth-token")})
-
 (defn -main
   [& _]
   (log/info "Starting tekton-watcher...")
-  (api/start-messaging publishers subscribers (read-config))
+  (api/start-messaging publishers subscribers (config/read-config))
   (log/info "tekton-watcher started")
   (.. Thread currentThread join))
